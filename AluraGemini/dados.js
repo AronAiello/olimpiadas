e7dffe2f41f8e4bc26434b9e6aa60f9ed12b222b
Aron Aiello <52681016+AronAiello@users.noreply.github.com>
2024-09-07 03:16:59 -0300

    Add files via upload

---
+++ AluraGemini/dados.js
@@ -0,0 +1,20 @@
+let dados = [
+  {
+    titulo: "Rayssa Leal",
+    descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos 13 anos, tornou-se a mais jovem medalhista olímpica da história do Brasil.",
+    link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+    tags: "skate"
+  },
+  {
+    titulo: "Rebeca Andrade",
+    descricao: "Rebeca Andrade é uma das maiores atletas brasileiras de todos os tempos. Com suas habilidades excepcionais e determinação inabalável, ela conquistou o mundo da ginástica artística e se tornou um verdadeiro símbolo de superação.",
+    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+    tags: "ginastica"
+  },
+  {
+    titulo: "Beatriz Souza",
+    descricao: "Beatriz Souza é uma judoca brasileira que conquistou a medalha de ouro nas Olimpíadas de Paris. Sua história de superação e dedicação ao esporte a tornou um exemplo para muitos.",
+    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+    tags: "judo judoca"
+  }
+];
